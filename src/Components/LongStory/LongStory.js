8239726def05628f640fb2f0b6b6432e6dda7a1c
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-08-19 22:42:58 +0600

    long story updated with author name ,pic,creation and hashtags

--- src/Components/LongStory/LongStory.js
+++ src/Components/LongStory/LongStory.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import LoadStory from '../CustomHooks/LoadStory';
-
+import image from './../../Images/author.jpg'
+import shape from './../../Images/trouble.svg'
+import './LongStory.css'
 const LongStory = () => {
     const [story]=LoadStory();
     // const [story,setStory]=useState([]);
@@ -10,15 +12,34 @@ const LongStory = () => {
     //     .then(data=>setStory(data))
     // },[])
    const expectedStory=story?.filter(s=>s?.type==="long");
+   let hashtag=expectedStory[0]?.hashtags.join(' '); 
+   hashtag=hashtag?.slice(0,32)+'.....';
+   console.log(hashtag);
    //const desiredStory=expectedStory[0];
     return (
-        <div>
-           <h2>{expectedStory[0]?.title}</h2>
-          
+        <div className='longStory'>
+           <h2 className='title'>{expectedStory[0]?.title}</h2>
+           <div className='mainSection'>
+           <div>
+         <img className='authorImg' src={image} alt="" />
+          <p  id='authorName'>{expectedStory[0]?.author?.name}</p>
+         </div>
+         <div>
+            <img className='shapeImg' src={shape} alt="" srcset="" />
+            <p>{expectedStory[0]?.community?.name}</p>
+         </div>
+           </div>
+           <hr />
+
+           <div className='extraSec'>
+<p id='created'>{expectedStory[0]?.created}</p>
+<p>{hashtag}</p>
+           </div>
+        
            {/* {
             expectedStory.map(s=><p>{s.title}</p>)
            } */}
-<p>Story:{story?.length}</p>
+
         </div>
     );
 };
